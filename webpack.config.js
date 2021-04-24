0fde3a79d8ab69fc6600f585291303b295e2e958
Yury <ymorpheus@gmail.com>
2021-04-24 13:56:01 +0300

    refactor

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  devServer: {
-    http2: true,
-    https: {
-      key: fs.readFileSync('./webhook_pkey.key'),
-      cert: fs.readFileSync('./webhook_cert.pem'),
-    },
-  },
-};
