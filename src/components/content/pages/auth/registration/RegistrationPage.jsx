12a01436a797da34c376b05ca5b99f1e981e7727
Yury <ymorpheus@gmail.com>
2021-05-17 15:10:44 +0300

    refactor: rewrite registrationpage in functional style

--- src/components/content/pages/auth/registration/RegistrationPage.jsx
+++ src/components/content/pages/auth/registration/RegistrationPage.jsx
@@ -10,114 +10,106 @@ import { registerUser } from '../../../../../store/current-user/actions';
 import Loader from '../../../../common/loader/Loader';
 import ActionStatus from '../../../../../constants/action-status';
 
-class RegistrationPage extends Component {
-  constructor() {
-    super();
+function RegistrationPage(props) {
+  const { registerUser, authErrors, status } = props;
 
-    this.validationSchema = Yup.object().shape({
-      email: Yup
-        .string()
-        .email('Email invalid format.')
-        .required('Email cannot be empty.'),
-      password: Yup
-        .string()
-        .min(8, `Password should contain at least ${MIN_PASSWORD_LENGTH} characters`)
-        .matches(PASSWORD_REGEX, 'Password invalid format.')
-        .required('Password can not be empty.'),
-      repeatPass: Yup
-        .string()
-        .oneOf([Yup.ref('password'), null], 'Passwords does not match.')
-    });
-  }
+  const validationSchema = Yup.object().shape({
+    email: Yup
+      .string()
+      .email('Email invalid format.')
+      .required('Email cannot be empty.'),
+    password: Yup
+      .string()
+      .min(8, `Password should contain at least ${MIN_PASSWORD_LENGTH} characters`)
+      .matches(PASSWORD_REGEX, 'Password invalid format.')
+      .required('Password can not be empty.'),
+    repeatPass: Yup
+      .string()
+      .oneOf([Yup.ref('password'), null], 'Passwords does not match.')
+  });
 
-  submitForm = async (values) => {
-    const { registerUser } = this.props;
-
+  const submitForm = async (values) => {
     await registerUser(values);
   }
 
-  render() {
-    const { authErrors, status } = this.props;
+  return (
+    <div className="content__registration-page auth-page first-layer-card">
+      <h2 className="registration-page__heading auth-page__heading card-heading">Create new account</h2>
 
-    return (
-      <div className="content__registration-page auth-page first-layer-card">
-        <h2 className="registration-page__heading auth-page__heading card-heading">Create new account</h2>
+      <Formik
+        initialValues={{ email: '', password: '', repeatPass: '' }}
+        onSubmit={submitForm}
+        validationSchema={validationSchema}
+      >
+        {({ handleSubmit, handleChange, values, errors, touched }) => (
+          <form
+            className="registration-page__form auth-page__form second-layer-card"
+            onSubmit={handleSubmit}
+          >
 
-        <Formik
-          initialValues={{ email: '', password: '', repeatPass: '' }}
-          onSubmit={this.submitForm}
-          validationSchema={this.validationSchema}
-        >
-          {({ handleSubmit, handleChange, values, errors, touched }) => (
-            <form
-              className="registration-page__form auth-page__form second-layer-card"
-              onSubmit={handleSubmit}
-            >
+            <label className="input__label">
+              Email
 
-              <label className="input__label">
-                Email
+              <input
+                name="email"
+                type="text"
+                onChange={handleChange}
+                value={values.email}
+                className="form__input form__input_email"
+                autoFocus
+              />
+            </label>
 
-                <input
-                  name="email"
-                  type="text"
-                  onChange={handleChange}
-                  value={values.email}
-                  className="form__input form__input_email"
-                  autoFocus
-                />
-              </label>
+            {(errors.email && touched.email) || authErrors ? (
+              <ValidationErrMsg errorMsg={errors.email || authErrors} />
+            ) : null}
 
-              {(errors.email && touched.email) || authErrors ? (
-                <ValidationErrMsg errorMsg={errors.email || authErrors} />
-              ) : null}
+            <label className="input__label">
+              Password
 
-              <label className="input__label">
-                Password
+              <input
+                name="password"
+                type="password"
+                onChange={handleChange}
+                value={values.password}
+                className="form__input form__input_password"
+                autoComplete="on"
+              />
+            </label>
 
-                <input
-                  name="password"
-                  type="password"
-                  onChange={handleChange}
-                  value={values.password}
-                  className="form__input form__input_password"
-                  autoComplete="on"
-                />
-              </label>
+            {errors.password && touched.password ? (
+              <ValidationErrMsg errorMsg={errors.password} />
+            ) : null}
 
-              {errors.password && touched.password ? (
-                <ValidationErrMsg errorMsg={errors.password} />
-              ) : null}
+            <label className="input__label">
+              Repeat your password
 
-              <label className="input__label">
-                Repeat your password
+              <input
+                name='repeatPass'
+                type='password'
+                onChange={handleChange}
+                value={values.repeatPass}
+                className="form__input form__input_password"
+                autoComplete="on"
+              />
+            </label>
 
-                <input
-                  name='repeatPass'
-                  type='password'
-                  onChange={handleChange}
-                  value={values.repeatPass}
-                  className="form__input form__input_password"
-                  autoComplete="on"
-                />
-              </label>
+            {errors.repeatPass && touched.repeatPass ? (
+              <ValidationErrMsg errorMsg={errors.repeatPass} />
+            ) : null}
 
-              {errors.repeatPass && touched.repeatPass ? (
-                <ValidationErrMsg errorMsg={errors.repeatPass} />
-              ) : null}
+            <section className="registration-page__controls form__controls">
+              <button type="submit" className="registration-page__submit submit-btn">Create account</button>
+              or
+              <Link to='sign-in' className="registration-page__cancel cancel-btn">Sign in</Link>
+            </section>
+          </form>
+        )}
+      </Formik>
 
-              <section className="registration-page__controls form__controls">
-                <button type="submit" className="registration-page__submit submit-btn">Create account</button>
-                or
-                <Link to='sign-in' className="registration-page__cancel cancel-btn">Sign in</Link>
-              </section>
-            </form>
-          )}
-        </Formik>
-
-        {status === ActionStatus.LOADING && <Loader />}
-      </div>
-    )
-  }
+      {status === ActionStatus.LOADING && <Loader />}
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
