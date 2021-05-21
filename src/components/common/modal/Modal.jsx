359c8b071367a9e14b34ee499934b96274fc7bd6
yurykurouski <ymorpheus@gmail.com>
2021-05-22 00:27:48 +0300

    refactor: refactored modal window

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -1,20 +1,16 @@
 import React, { useCallback, useEffect } from "react";
 import ReactDom from "react-dom";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
-import { Formik } from "formik";
 
-import Input from "../input/Input";
 import "./modal.css";
 import { ESCAPE_KEYCODE } from "../../../constants";
 
 export default function Modal(
   {
+    component: Component,
     heading,
-    fields,
     handleCloseModal,
-    handleModalSubmit,
-    validationSchema,
-    btnText,
+    ...rest
   }
 ) {
   const modalRoot = document.getElementById('modal-root');
@@ -45,47 +41,13 @@ export default function Modal(
     };
   }, [escFunction]);
 
-  const getValues = () => {
-    let vals = {};
-
-    fields.forEach((el) => {
-      vals[el.name] = el.value;
-    });
-    return vals;
-  };
-
   return ReactDom.createPortal(
     <ClickAwayListener onClickAway={handleCloseModal}>
       <div className="modal-window first-layer-card">
         <h3 className="modal-window__heading">{heading}</h3>
 
-        <Formik
-          initialValues={getValues()}
-          onSubmit={handleModalSubmit}
-          validationSchema={validationSchema}
-        >
-          {({ handleSubmit, handleChange, values, errors }) => (
-            <form
-              className="modal-window__main second-layer-card"
-              onSubmit={handleSubmit}
-            >
-              {fields.map((field) => (
-                <Input
-                  {...field}
-                  value={values[field.name]}
-                  handleChange={handleChange}
-                  key={field.name}
-                  error={errors[field.name]}
-                  touched={values}
-                />
-              ))}
+        {Component}
 
-              <button type="submit" className="modal__submit-btn submit-btn">
-                {btnText}
-              </button>
-            </form>
-          )}
-        </Formik>
       </div>
     </ClickAwayListener>,
     el
