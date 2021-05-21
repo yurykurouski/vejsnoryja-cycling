3a4b53bbf056e34fad5b7937b8155dbeb711b917
yurykurouski <ymorpheus@gmail.com>
2021-05-22 01:30:06 +0300

    refactor: addButton

--- src/components/common/add-button/AddButton.jsx
+++ src/components/common/add-button/AddButton.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import NewEvent from "./actions/NewEvent";
-import './add-button.css';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+
+import NewEvent from "./actions/NewEvent";
+import AddButtonActions from '../../../constants/add-button-actions';
+import './add-button.css';
 
 export default class AddButton extends Component {
   constructor() {
@@ -26,30 +28,27 @@ export default class AddButton extends Component {
     this.setState({
       clickedClass: '',
       expanded: false
-    })
+    });
   }
 
   render() {
     const { clickedClass, expanded } = this.state;
 
     return (
-
       <div className='add-btn_container'>
-        <NewEvent
-          name='Smthng'
-          expanded={expanded}
-          type='create_new_folder'
-          linkTo='register'
-        />
-        <NewEvent
-          name='New event'
-          expanded={expanded}
-          type='edit'
-          linkTo='new-event'
-        />
+
+        {AddButtonActions.ACTIONS.map((action) => (
+          <NewEvent
+            name={action.name}
+            expanded={expanded}
+            type={action.type}
+            linkTo={action.linkTo}
+          />
+        ))}
+
         <ClickAwayListener onClickAway={this.handleClickAway}>
           <button className='add-btn' onClick={this.handleClick}>
-            <svg className={`add-btn__add-icon ${clickedClass}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <svg className={`add-btn__add-icon ${ clickedClass }`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
             </svg>
