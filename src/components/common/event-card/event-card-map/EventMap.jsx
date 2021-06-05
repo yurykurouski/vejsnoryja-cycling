268ea24b01cc62ba93b20d9816e7fc9a16a2e9a2
yurykurouski <ymorpheus@gmail.com>
2021-06-05 15:57:36 +0300

    fix: rewrite eventMap to functional component

--- src/components/common/event-card/event-card-map/EventMap.jsx
+++ src/components/common/event-card/event-card-map/EventMap.jsx
@@ -1,53 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
 import './event-map.css';
-// ! переписать
-export default class EventMap extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      center: {
-        lat: 53.68312602177623,
-        lng: 23.83411884664919
-      },
-    }
-  }
+const center = {
+  lat: 53.68312602177623,
+  lng: 23.83411884664919
+}
 
-  render() {
-    const { center } = this.state;
-    const { isDraggable, addMarker, updateMarker, markerData, mainClass } = this.props;
+export default function EventMap(props) {
+  const { isDraggable, addMarker, updateMarker, markerData, mainClass } = props;
 
-    return (
-      <MapContainer
-        className={`${ mainClass } map second-layer-card`}
-        center={markerData ? markerData : center}
-        zoom={13}
-        zoomControl={false}
-        scrollWheelZoom={true}
-        whenReady={(map) => {
-          if (addMarker) {
-            map.target.on("click", function (e) {
-              addMarker(e);
-            });
+  return (
+    <MapContainer
+      className={`${ mainClass } map second-layer-card`}
+      center={markerData ? markerData : center}
+      zoom={13}
+      zoomControl={false}
+      scrollWheelZoom={true}
+      whenReady={(map) => {
+        if (addMarker) {
+          map.target.on("click", function (e) {
+            addMarker(e);
+          });
+        }
+      }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {markerData && <Marker
+        position={markerData}
+        draggable={isDraggable}
+        eventHandlers={{
+          dragend(e) {
+            updateMarker(e);
           }
         }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {markerData && <Marker
-          position={markerData}
-          draggable={isDraggable}
-          eventHandlers={{
-            dragend(e) {
-              updateMarker(e);
-            }
-          }}
-        />}
-      </MapContainer>
-    )
-  }
+      />}
+    </MapContainer>
+  )
 }
